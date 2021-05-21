af139d31d4bc51cb6940c9a06ffd6880ca725380
水汐 <58126937+Mizushio@users.noreply.github.com>
2021-05-21 14:40:12 +0800

    get&post

--- pages/wxml/index.js
+++ pages/wxml/index.js
@@ -6,7 +6,10 @@ Page({
    */
   data: {
     app: getApp(),//app.js里的globalData
-    time: (new Date()).toString()
+    time: (new Date()).toString(),
+    getdata:"1",
+    postdata:"2",
+    // data2:"?????",
   },
 
 //   logBtn: function(options){
@@ -30,7 +33,7 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+      
   },
 
   /**
@@ -73,5 +76,92 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  }  ,
+
+  getbaidu:function(){
+    var that=this
+    let promise = new Promise(function (resolve, reject) {
+      wx.request({
+        url: "http://mizushio.top",
+        header: {'content-type': 'application/json'},
+        data: {'k':'l'},
+        method: 'GET',
+        success: function (res) {
+          //返回结果自行处理
+          console.log('返回结果：')
+          // data2=res.data
+          // that.data.getdata=res.data
+          that.setData({
+            getdata:res.data
+          })
+          console.log(res.data)
+          // app.netWorkData.result = res.data
+          resolve();
+  
+        }
+      })
+    });
+  },
+
+  postto:function(){
+  var that=this
+    wx.request({
+      url: "http://mizushio.top:8080/QQsendGroupMessage",
+      header: {'content-type': 'application/json'},
+      data: {'groupid':'985100249',"msg":"百度，日常测试，无视就行"},
+      method: 'POST',
+      success: function (res) {
+        //自行处理返回结果
+        console.log('返回结果：')
+        console.log(res.data)
+        that.setData({
+          postdata:res.data
+        })
+        // app.netWorkData.result = res.data
+        resolve();
+      }
+
+    })
+  },
 })
+
+//post请求 url：请求路径，请求header，params请求参数，app全局变量
+function networkpost(url, headers, params, app) {
+  let promise = new Promise(function (resolve, reject) {
+    wx.request({
+      url: url,
+      header: headers,
+      data: params,
+      method: 'POST',
+      success: function (res) {
+        //自行处理返回结果
+        console.log('返回结果：')
+        console.log(res.data)
+        // app.netWorkData.result = res.data
+        resolve();
+      }
+
+    })
+});
+return promise;
+}
+//get请求
+function networkget(url, headers, params, app) {
+  let promise = new Promise(function (resolve, reject) {
+    wx.request({
+      url: url,
+      header: headers,
+      data: params,
+      method: 'GET',
+      success: function (res) {
+        //返回结果自行处理
+        console.log('返回结果：')
+        console.log(res.data)
+        app.netWorkData.result = res.data
+        resolve();
+
+      }
+    })
+  });
+  return promise;
+}   
